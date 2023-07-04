eec3694102e04573403068e22978b697ab25c11d
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 19:37:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # read csv from a github repo
 df = pd.read_csv("https://raw.githubusercontent.com/akorinsichris/eclaro/main/resource.csv")
@@ -33,7 +34,30 @@ account=st.sidebar.multiselect("Filter By Account:",
 
 selection_query=df.query("ACCT_NAME==@account")
 
-st.dataframe(selection_query)
+# st.dataframe(selection_query)
+
+account_per_cnt = (selection_query.groupby(by=['ACCT_NAME']).count()['EID'])
+
+account_per_cnt_barchart=px.bar(account_per_cnt,
+                                x='EID',
+                                y=account_per_cnt.index,
+                                title='Count per Account',
+                                color_discrete_sequence=["#17f50c"],
+                               )
+account_per_cnt_barchart.update_layout(plot_bgcolor = "rgba(0,0,0,0)",xaxis=(dict(showgrid=False)))
+
+account_per_cnt_piechart = px.pie(account_per_cnt, 
+                                  names= account_per_cnt.index,
+                                  values ="EID",
+                                  title="Count per Account",
+                                  hole=.3,
+                                  color=account_per_cnt.index,
+                                  color_discrete_sequence=px.colors.sequential.RdPu_r
+                                 )
+
+left_column,right_column=st.columns(2)
+left_column.plotly_chart(account_per_cnt_barchart,use_container_width=True)
+right_column.plotly_chart(account_per_cnt_piechart,use_container_width=True)
 
 
 
