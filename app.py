3175ae6d8d8d64e6b4235cc8f8526053bb41dcf5
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 15:05:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 # read csv from a github repo
 df = pd.read_csv("https://raw.githubusercontent.com/akorinsichris/eclaro/main/resource_account.csv")
 
-
 st.sidebar.header("Filter By:")
 
 account=st.sidebar.multiselect("Filter By Account:",
@@ -13,7 +12,21 @@ account=st.sidebar.multiselect("Filter By Account:",
 
 selection_query=df.query("ACCT_NAME==@account")
 
-st.dataframe(selection_query)
+#st.dataframe(selection_query)
+st.title("GSD Dashboard")
+
+total_count=(selection_query["ACCT_NAME"].count())
+total_resource=(selection_query["EID"].count())
+
+col1,col2=st.columns(2)
+
+with col1:
+  st.markdown("### Account Count:")
+  st.subheader(f'{total_count}')
+with col2:
+  st.markdown("### Resource Count:")
+  st.subheader(f'{total_resource}')
+
 
 
 
