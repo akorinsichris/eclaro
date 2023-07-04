010b576140d8f5e29216387701e00e4b637196f6
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 14:28:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
-import snowflake.connector as sf
-import pandas as pd
-from datetime import date
 
-conn = sf.connect(**st.secrets["snowflake"])
-my_data_rows = conn.query('Select * from resource_account')
-st.dataframe(my_data_rows)
+# initialize connection
+conn = st.experimental_connection('snowflake', type='sql')
+
+#load the table as a dataframe using the snowpark session.
+@st.cache_date
+def load_table():
+  with conn.safe_session() as session:
+    return session.table('resource_account').to_pandas()
+    
+df = load_table()
+
+st.dataframe(df)
 
 
 
