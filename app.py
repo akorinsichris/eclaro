3ac7aca51577363c2e7cb6e97ff958bb8347c28a
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 18:01:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("GSD Dashboard")
 
 total_count=df["EID"].count()
 total_resource=df["ACCT_NAME"].count()
-total_manager=df["ROLE"].count()
+total_manager=len(df[df["ROLE"] == 'Manager'])
 
 col1,col2,col3=st.columns(3)
 
