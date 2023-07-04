502efabc20dfc5ca445237b79508c71dc9dd4a6e
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-04 13:59:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import snowflake.connector as sf
 import pandas as pd
 from datetime import date
 
-conn = sf.connect(**streamlit.secrets["snowflake"])
+conn = sf.connect(**st.secrets["snowflake"])
 my_data_rows = conn.query('Select * from resource_account')
 st.dataframe(my_data_rows)
 
